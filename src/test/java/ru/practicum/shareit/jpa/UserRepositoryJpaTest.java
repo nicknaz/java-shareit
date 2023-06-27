a518303dbba373cd2bf70eec3225c10e069feec2
NNazarov <kolya.nazarov41@gmail.com>
2023-06-28 00:45:32 +0300

    fix "tests"

--- src/test/java/ru/practicum/shareit/jpa/UserRepositoryJpaTest.java
+++ src/test/java/ru/practicum/shareit/jpa/UserRepositoryJpaTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.dao.DataIntegrityViolationException;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.repository.UserRepositoryJPA;
 
