4aca7d7c3611f2ec96d9e2dcc4506e33fa513ed6
NNazarov <kolya.nazarov41@gmail.com>
2023-06-29 23:12:29 +0300

    test

---
+++ server/src/main/java/ru/practicum/shareit/exception/InvalidDateException.java
@@ -0,0 +1,9 @@
+package ru.practicum.shareit.exception;
+
+public class InvalidDateException extends RuntimeException {
+
+    public InvalidDateException(String message) {
+        super(message);
+    }
+
+}
